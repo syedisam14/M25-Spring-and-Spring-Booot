bc86b9871cd9c28abc5efb286234d2fce2f19c01
cw user <cw user@DESKTOP-DFQ4JQ3>
2022-12-03 11:26:41 -0800

    Commit

---
+++ PalcementmMang/src/main/java/com/example/demo/PalcementmMangApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalcementmMangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalcementmMangApplication.class, args);
+	}
+
+}
